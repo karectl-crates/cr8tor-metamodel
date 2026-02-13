ba531d91cbb81326ced5582a571d1d192f0c429b
Michael Harding <m.harding@lancaster.ac.uk>
2026-02-13 12:50:41 +0000

    Update data model

--- src/cr8tor_metamodel/datamodel/cr8tor_metamodel.py
+++ src/cr8tor_metamodel/datamodel/cr8tor_metamodel.py
@@ -1,5 +1,5 @@
 # Auto generated from cr8tor_metamodel.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-02-13T12:36:39
+# Generation date: 2026-02-13T12:47:08
 # Schema: cr8tor-metamodel
 #
 # id: https://w3id.org/karectl-crates/cr8tor-metamodel
